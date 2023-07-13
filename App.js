94adbc60651f51bae6210d9513b23d968356ac16
Alex Mendoza <amendoza807@gmail.com>
2023-07-12 21:00:47 -0500

    inicio de app

---
+++ App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {SafeAreaView, StyleSheet, Text} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import Home from './src/screens/Home';
+import Menu from './src/screens/Menu';
+
+const Stack = createNativeStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="home">
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{title: 'Welcome'}}
+        />
+        <Stack.Screen name="Menu" component={Menu} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
